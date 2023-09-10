38b1f0c9f965b34f54502442d7d0df2ea8a6b7fc
Ovais Raza <rovais53@gmail.com>
2023-09-10 22:54:14 +0500

    a few changes done in the frontend

--- app/page.js
+++ app/page.js
@@ -6,7 +6,7 @@ const page = () => {
     <>
       <section className="text-center">
         <div className="conatiner mt-16">
-          <h1 className="text-8xl font-bold my-5">Hunting</h1>
+          <h1 className="text-7xl font-bold my-5">Hunting</h1>
           <h1 className="leading-3 text-5xl font-semibold">Coder</h1>
           <p className="text-lg my-7 text-zinc-700 font-medium">A Blog By a Coder</p>
         </div>
